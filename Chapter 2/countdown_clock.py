3c09ab5b7537c4a766b38eb100fc199a016799c3
Jared Gerschler <jjgerschler@gmail.com>
2018-10-01 12:46:25 -0500

    countdown clock is in progress!

--- Chapter 2/countdown_clock.py
+++ Chapter 2/countdown_clock.py
@@ -3,8 +3,10 @@ import pygame
 from random import randint
 
 WHITE = (255, 255, 255)
-# the color black
 BLACK = (0, 0, 0)
+
+#countdown time in seconds
+START_TIME = 300
 
 finished = False
 
@@ -18,33 +20,44 @@ pygame.display.update()
 def text_display(text):
     # our font is the pygame built-in, size 256
     text_font = pygame.font.Font(None, 256)
-    # our text surface is created by rendering the text
-    # using the chosen font
     text_surface = text_font.render(text, True, BLACK)
-    # get the rectangle for our text surface
     text_rectangle = text_surface.get_rect()
-    # fill the display with white
     game_display.fill(WHITE)
-    # center the text by centering its rectangle
     text_rectangle.center = (game_display.get_width()/2,
                              game_display.get_height()/2)
     #display the text
     game_display.blit(text_surface, text_rectangle)
-    # update the display
     pygame.display.update()
     
 while not finished:
+    
     for event in pygame.event.get():
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-            # if the key pressed is SPACE, send a random
-            # integer string to the text_display function
+            # if the key pressed is SPACE, start the timer
             if event.key == pygame.K_SPACE:
-                text_display(str(randint(1, 10)))
+                for t in range(START_TIME, -1, -1):
+                    minutes = t / 60
+                    seconds = t % 60
+                    sf = "{:02d}:{:02d}".format(*divmod(t, 60))               
+                    game_display.fill(WHITE)
+                    text_display(sf)
+                    pygame.display.update()
         if event.type == pygame.QUIT:
             finished = True
+
+    if time - pygame.time.get_ticks() >= 1000:
+        clock_time -= 1
+        time = pygame.time.get_ticks()
+
+    if clock_time == 0:
+        finished = True
+
+    game_display.fill(WHITE)
+    text_display(sf)
+    pygame.display.update()
       
 pygame.quit()
 sys.exit()
